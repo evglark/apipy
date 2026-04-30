517011613db5510b94f124d336720e9d8e015ad5
Eugene Larkin <31957364+evglark@users.noreply.github.com>
2026-04-30 19:18:29 +0200

    Fix users router import and remove unused test fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,11 +3,6 @@ import pytest
 from apipy.storage import reset_state_with_seeds
 
 
-@pytest.fixture
-def db():
-    return []
-
-
 @pytest.fixture(autouse=True)
 def reset_state():
     reset_state_with_seeds()
